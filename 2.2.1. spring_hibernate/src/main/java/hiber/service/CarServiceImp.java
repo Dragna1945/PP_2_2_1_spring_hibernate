4c78186e086557ad3bea50f2216cd17ed1f4d683
YaroslavShcherbakov <dragna1945@gmail.com>
2023-07-20 11:56:59 +0300

    работает

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImp.java
@@ -0,0 +1,16 @@
+package hiber.service;
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+public class CarServiceImp implements CarService {
+    @Override
+    public void add(Car car) {
+
+    }
+
+    @Override
+    public List<User> listCars() {
+        return null;
+    }
+}
